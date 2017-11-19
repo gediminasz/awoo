6b0433ccf53dbfd758090acc2b6d35863b73d1ec
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2017-11-19 13:51:43 +0200

    Use page.html in index view

--- views/index.py
+++ views/index.py
@@ -8,9 +8,6 @@ class IndexView(BaseView):
     def get(self):
         profile = spotify.profile()
         if profile:
-            return self._render_index(profile)
+            return flask.render_template('page.html', profile=profile)
         else:
             return flask.render_template('login.html')
-
-    def _render_index(self, profile):
-        return flask.render_template('index.html', profile=profile)
