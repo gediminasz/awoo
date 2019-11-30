5f43573a4e3136ffd151417d554ddd7386b64993
Gediminas Zlatkus <gediminas.zlatkus@gmail.com>
2019-11-30 13:14:57 +0200

    Add preview controls

--- static/js/vue.js
+++ static/js/vue.js
@@ -9,26 +9,53 @@ const TABS = {
 
 let app = new Vue({
     el: '#app',
+
     data: {
         tabs: TABS,
         currentTab: TAB_LONG_TERM,
         tracks: {},
+        nowPlaying: { track: null, audio: null }
     },
+
     computed: {
         displayedTracks: function () { return this.tracks[this.currentTab] }
     },
+
     methods: {
         switchTab: function (tab) {
             this.currentTab = tab;
             this.fetchTracks(tab);
         },
+
         fetchTracks: function (tab) {
             if (this.tracks[tab]) return;
             fetch('/tracks?time_range=' + tab, { credentials: "same-origin" })
                 .then((response) => response.json())
                 .then((data) => Vue.set(this.tracks, tab, data));
         },
+
+        playPreview: function (track) {
+            this.stopPlayback();
+
+            const audio = new Audio(track.preview_url);
+            audio.play()
+            audio.addEventListener('ended', () => this.stopPreview());
+
+            this.nowPlaying = { track, audio };
+        },
+
+        stopPreview: function () {
+            this.stopPlayback();
+            this.nowPlaying = { track: null, audio: null };
+        },
+
+        stopPlayback: function () {
+            if (this.nowPlaying.audio) {
+                this.nowPlaying.audio.pause();
+            }
+        }
     },
+
     mounted: function () {
         this.fetchTracks(this.currentTab);
     }
